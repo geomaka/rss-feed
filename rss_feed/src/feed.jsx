8411215cc855a310dba67b177f52a7d246dd9c6a
George Maina <107925640+geomaka@users.noreply.github.com>
2024-04-26 01:20:15 +0300

    changes to the date format

--- rss_feed/src/feed.jsx
+++ rss_feed/src/feed.jsx
@@ -1,12 +1,11 @@
 const Feed= function ({title, link, date}){
-    let formatted = {date:"numeric", month:"long",year:"numeric"}
-    let articleDate = new Date(date).toLocaleDateString("en-GB",formatted)
+ 
     return (
         <>
             <a href={link} target="_blank" rel="noopener noreferrer"
             className="hover:opacity-70 hover:text-orange-500"> 
             <h3  className="text-xl mb-l">{title}</h3>
-            <p>{articleDate}</p>
+            <p>{date}</p>
             </a>
             <hr/>
         </>
